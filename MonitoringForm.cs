f2386beacc0265b1089fc78d718ee7aeb6722e4c
Jimmuel <jimmuelsanggayan@gmail.com>
2025-08-23 19:15:47 +0800

    Initial Monitoring Form

---
+++ MonitoringForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartCodeLab
+{
+    public partial class MonitoringForm : Form
+    {
+        public MonitoringForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MonitoringForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMenu2_Click(object sender, EventArgs e)
+        {
+            student_Panel.Visible = true;
+            files_Panel.Visible = false;
+            instructions_Panel.Visible = false;
+        }
+
+        private void btnMenu1_Click(object sender, EventArgs e)
+        {
+            student_Panel.Visible = false;
+            files_Panel.Visible = true;
+            instructions_Panel.Visible = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            instructions_Panel.Visible = true;
+            student_Panel.Visible = false;
+            files_Panel.Visible = false;
+        }
+    }
+}
