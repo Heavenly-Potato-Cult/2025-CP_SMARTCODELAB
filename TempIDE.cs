777d23fedc3de500d22e68778ff491ca5dc84331
Jimmuel <none>
2025-10-15 16:59:59 +0800

    temp IDE

--- TempIDE.cs
+++ TempIDE.cs
@@ -15,16 +15,39 @@ namespace SmartCodeLab
         public TempIDE()
         {
             InitializeComponent();
+
         }
 
         private void TempIDE_Load_1(object sender, EventArgs e)
         {
-
+           
         }
 
         private void codeEditor1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void ResizeTabs()
+        {
+            if (tabControl_RightSide.TabPages.Count == 0) return;
+
+            int tabCount = tabControl_RightSide.TabPages.Count;
+            int totalWidth = tabControl_RightSide.ClientSize.Width;
+            int tabWidth = totalWidth / tabCount;
+
+            tabControl_RightSide.SizeMode = TabSizeMode.Fixed;
+            tabControl_RightSide.ItemSize = new Size(tabWidth, 30); // 30 = tab height
+        }
+
+        private void tabControl_RightSide_Resize(object sender, EventArgs e)
+        {
+            ResizeTabs();
+        }
+
+        private void TempIDE_Shown(object sender, EventArgs e)
+        {
+            ResizeTabs();
         }
     }
 }
