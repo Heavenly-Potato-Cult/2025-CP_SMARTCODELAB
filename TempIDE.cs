17114af89a230122fb915be2fadf2a71ce4f448f
Jimmuel <none>
2025-10-15 17:22:20 +0800

    fix tab wrapping

--- TempIDE.cs
+++ TempIDE.cs
@@ -46,7 +46,7 @@ namespace SmartCodeLab
             try
             {
                 int tabCount = tabControl_RightSide.TabPages.Count;
-                int tabWidth = totalWidth / tabCount;
+                int tabWidth = (totalWidth / tabCount) - 2;
 
                 tabControl_RightSide.SizeMode = TabSizeMode.Fixed;
                 tabControl_RightSide.ItemSize = new Size(tabWidth, 30); // 30 = tab height
