5a7c2d1e429522619c99e714780f7b7dd443c84f
StagnantPotato <jimmuelsanggayan@gmail.com>
2025-10-27 21:08:29 +0800

    Code Quality Choises2

---
+++ CustomComponents/GeneralComponents/CodeQualityChoices2.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace SmartCodeLab.CustomComponents.GeneralComponents
+{
+    partial class CodeQualityChoices2
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            logicalComplexityLabel = new Label();
+            efficiencyLabel = new Label();
+            accuracyLabel = new Label();
+            label14 = new Label();
+            efficiencyTrackBar = new TrackBar();
+            label11 = new Label();
+            logicalComplexityTrackBar = new TrackBar();
+            label2 = new Label();
+            readabilityTrackBar = new TrackBar();
+            label8 = new Label();
+            accuracyTrackBar = new TrackBar();
+            label13 = new Label();
+            readabilityLabel = new Label();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)efficiencyTrackBar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)logicalComplexityTrackBar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)readabilityTrackBar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)accuracyTrackBar).BeginInit();
+            SuspendLayout();
+            // 
+            // logicalComplexityLabel
+            // 
+            logicalComplexityLabel.AutoSize = true;
+            logicalComplexityLabel.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            logicalComplexityLabel.ForeColor = Color.DimGray;
+            logicalComplexityLabel.Location = new Point(339, 291);
+            logicalComplexityLabel.Name = "logicalComplexityLabel";
+            logicalComplexityLabel.Size = new Size(29, 20);
+            logicalComplexityLabel.TabIndex = 46;
+            logicalComplexityLabel.Text = "0%";
+            // 
+            // efficiencyLabel
+            // 
+            efficiencyLabel.AutoSize = true;
+            efficiencyLabel.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            efficiencyLabel.ForeColor = Color.DimGray;
+            efficiencyLabel.Location = new Point(339, 220);
+            efficiencyLabel.Name = "efficiencyLabel";
+            efficiencyLabel.Size = new Size(29, 20);
+            efficiencyLabel.TabIndex = 45;
+            efficiencyLabel.Text = "0%";
+            // 
+            // accuracyLabel
+            // 
+            accuracyLabel.AutoSize = true;
+            accuracyLabel.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            accuracyLabel.ForeColor = Color.DimGray;
+            accuracyLabel.Location = new Point(339, 78);
+            accuracyLabel.Name = "accuracyLabel";
+            accuracyLabel.Size = new Size(29, 20);
+            accuracyLabel.TabIndex = 44;
+            accuracyLabel.Text = "0%";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label14.ForeColor = Color.DimGray;
+            label14.Location = new Point(26, 220);
+            label14.Name = "label14";
+            label14.Size = new Size(72, 20);
+            label14.TabIndex = 43;
+            label14.Text = "Efficiency";
+            // 
+            // efficiencyTrackBar
+            // 
+            efficiencyTrackBar.Location = new Point(26, 243);
+            efficiencyTrackBar.Name = "efficiencyTrackBar";
+            efficiencyTrackBar.Size = new Size(342, 45);
+            efficiencyTrackBar.TabIndex = 42;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.ForeColor = Color.DimGray;
+            label11.Location = new Point(26, 291);
+            label11.Name = "label11";
+            label11.Size = new Size(136, 20);
+            label11.TabIndex = 41;
+            label11.Text = "Logical Complexity";
+            // 
+            // logicalComplexityTrackBar
+            // 
+            logicalComplexityTrackBar.Location = new Point(26, 314);
+            logicalComplexityTrackBar.Name = "logicalComplexityTrackBar";
+            logicalComplexityTrackBar.Size = new Size(342, 45);
+            logicalComplexityTrackBar.TabIndex = 40;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.DimGray;
+            label2.Location = new Point(26, 149);
+            label2.Name = "label2";
+            label2.Size = new Size(80, 20);
+            label2.TabIndex = 39;
+            label2.Text = "Readabilty";
+            // 
+            // readabilityTrackBar
+            // 
+            readabilityTrackBar.Location = new Point(26, 172);
+            readabilityTrackBar.Name = "readabilityTrackBar";
+            readabilityTrackBar.Size = new Size(342, 45);
+            readabilityTrackBar.TabIndex = 38;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.DimGray;
+            label8.Location = new Point(26, 78);
+            label8.Name = "label8";
+            label8.Size = new Size(107, 20);
+            label8.TabIndex = 37;
+            label8.Text = "Code Accuracy";
+            // 
+            // accuracyTrackBar
+            // 
+            accuracyTrackBar.Location = new Point(26, 101);
+            accuracyTrackBar.Name = "accuracyTrackBar";
+            accuracyTrackBar.Size = new Size(342, 45);
+            accuracyTrackBar.TabIndex = 36;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label13.ForeColor = Color.Black;
+            label13.Location = new Point(26, 23);
+            label13.Name = "label13";
+            label13.Size = new Size(158, 30);
+            label13.TabIndex = 35;
+            label13.Text = "Grading Criteria";
+            // 
+            // readabilityLabel
+            // 
+            readabilityLabel.AutoSize = true;
+            readabilityLabel.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            readabilityLabel.ForeColor = Color.DimGray;
+            readabilityLabel.Location = new Point(339, 149);
+            readabilityLabel.Name = "readabilityLabel";
+            readabilityLabel.Size = new Size(29, 20);
+            readabilityLabel.TabIndex = 47;
+            readabilityLabel.Text = "0%";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(139, 82);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(15, 14);
+            checkBox1.TabIndex = 48;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(112, 155);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(15, 14);
+            checkBox2.TabIndex = 49;
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(104, 226);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(15, 14);
+            checkBox3.TabIndex = 50;
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(168, 297);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(15, 14);
+            checkBox4.TabIndex = 51;
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // CodeQualityChoices2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            BorderRadius = 10;
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(readabilityLabel);
+            Controls.Add(logicalComplexityLabel);
+            Controls.Add(efficiencyLabel);
+            Controls.Add(accuracyLabel);
+            Controls.Add(label14);
+            Controls.Add(efficiencyTrackBar);
+            Controls.Add(label11);
+            Controls.Add(logicalComplexityTrackBar);
+            Controls.Add(label2);
+            Controls.Add(readabilityTrackBar);
+            Controls.Add(label8);
+            Controls.Add(accuracyTrackBar);
+            Controls.Add(label13);
+            Name = "CodeQualityChoices2";
+            Size = new Size(395, 383);
+            ((System.ComponentModel.ISupportInitialize)efficiencyTrackBar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)logicalComplexityTrackBar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)readabilityTrackBar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)accuracyTrackBar).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label logicalComplexityLabel;
+        private Label efficiencyLabel;
+        private Label accuracyLabel;
+        private Label label14;
+        private TrackBar efficiencyTrackBar;
+        private Label label11;
+        private TrackBar logicalComplexityTrackBar;
+        private Label label2;
+        private TrackBar readabilityTrackBar;
+        private Label label8;
+        private TrackBar accuracyTrackBar;
+        private Label label13;
+        private Label readabilityLabel;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+    }
+}
---
+++ CustomComponents/GeneralComponents/CodeQualityChoices2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartCodeLab.CustomComponents.GeneralComponents
+{
+    public partial class CodeQualityChoices2 : RoundedUserControl
+    {
+        public CodeQualityChoices2()
+        {
+            InitializeComponent();
+
+            SetupTrackBar(accuracyTrackBar, 80);
+            SetupTrackBar(readabilityTrackBar, 10);
+            SetupTrackBar(efficiencyTrackBar, 10);
+            SetupTrackBar(logicalComplexityTrackBar, 0);
+
+            InitializeGradingControls();
+
+        }
+
+        private void SetupTrackBar(TrackBar trackBar, int defaultValue = 0)
+        {
+
+            if (trackBar == null)
+            {
+                return;
+            }
+
+            trackBar.Minimum = 0;
+            trackBar.Maximum = 100;
+            trackBar.Value = defaultValue;
+        }
+
+        private void AnyTrackBar_ValueChanged(object sender, EventArgs e)
+        {
+
+            if (sender is not TrackBar trackBar)
+            {
+                return;
+            }
+
+
+            if (trackBar.Tag is not Label targetLabel)
+            {
+                return;
+            }
+
+            // 3. Update the label's text with the new value.
+            targetLabel.Text = $"{trackBar.Value}%";
+        }
+        private void InitializeGradingControls()
+        {
+            LinkTrackBarToLabel(accuracyTrackBar, accuracyLabel);
+            LinkTrackBarToLabel(readabilityTrackBar, readabilityLabel);
+            LinkTrackBarToLabel(efficiencyTrackBar, efficiencyLabel);
+            LinkTrackBarToLabel(logicalComplexityTrackBar, logicalComplexityLabel);
+        }
+
+        private void LinkTrackBarToLabel(TrackBar trackBar, Label label)
+        {
+
+            if (trackBar == null || label == null)
+            {
+                return;
+            }
+
+
+            trackBar.Tag = label;
+
+
+            trackBar.ValueChanged += AnyTrackBar_ValueChanged;
+        }
+    }
+}
