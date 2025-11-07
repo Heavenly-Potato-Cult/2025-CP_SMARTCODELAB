5f0466980d3059b91620402a757965d14b7b5dd5
StagnantPotato <jimmuelsanggayan@gmail.com>
2025-11-08 00:41:56 +0800

    ye

--- CustomComponents/ServerPageComponents/ExerciseManagerComponents/TestCase2.cs
+++ CustomComponents/ServerPageComponents/ExerciseManagerComponents/TestCase2.cs
@@ -16,5 +16,24 @@ namespace SmartCodeLab.CustomComponents.ServerPageComponents.ExerciseManagerComp
         {
             InitializeComponent();
         }
+
+        public TestCase2(KeyValuePair<string, string> testCase)
+        {
+            InitializeComponent();
+            codeEditor1.Text = testCase.Key;
+            codeEditor2.Text = testCase.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        
+
+        public KeyValuePair<string, string> GetTestCase()
+        {
+            return new KeyValuePair<string, string>(codeEditor1.Text, codeEditor2.Text);
+        }
     }
 }
