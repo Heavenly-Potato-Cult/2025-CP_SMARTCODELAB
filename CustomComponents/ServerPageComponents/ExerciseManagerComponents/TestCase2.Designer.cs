572bb79fb462ac146fd747852bd54f78f289c9a1
StagnantPotato <jimmuelsanggayan@gmail.com>
2025-11-24 04:21:44 +0800

    testcase2 ui update

--- CustomComponents/ServerPageComponents/ExerciseManagerComponents/TestCase2.Designer.cs
+++ CustomComponents/ServerPageComponents/ExerciseManagerComponents/TestCase2.Designer.cs
@@ -48,9 +48,9 @@
             // 
             // splitContainer1
             // 
-            splitContainer1.Dock = DockStyle.Top;
+            splitContainer1.Dock = DockStyle.Fill;
             splitContainer1.Location = new Point(0, 0);
-            splitContainer1.Margin = new Padding(3, 25, 3, 3);
+            splitContainer1.Margin = new Padding(3, 33, 3, 4);
             splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -62,9 +62,9 @@
             // 
             splitContainer1.Panel2.Controls.Add(codeEditor2);
             splitContainer1.Panel2.Controls.Add(label2);
-            splitContainer1.Size = new Size(727, 119);
-            splitContainer1.SplitterDistance = 329;
-            splitContainer1.SplitterWidth = 10;
+            splitContainer1.Size = new Size(649, 190);
+            splitContainer1.SplitterDistance = 315;
+            splitContainer1.SplitterWidth = 11;
             splitContainer1.TabIndex = 85;
             // 
             // codeEditor1
@@ -82,23 +82,22 @@
     '\'',
     '\''
     };
-            codeEditor1.AutoScrollMinSize = new Size(2, 14);
+            codeEditor1.AutoScrollMinSize = new Size(31, 18);
             codeEditor1.BackBrush = null;
-            codeEditor1.CharHeight = 14;
-            codeEditor1.CharWidth = 8;
+            codeEditor1.CharHeight = 18;
+            codeEditor1.CharWidth = 10;
             codeEditor1.DisabledColor = Color.FromArgb(100, 180, 180, 180);
             codeEditor1.Dock = DockStyle.Fill;
             codeEditor1.Font = new Font("Courier New", 9.75F);
             codeEditor1.ForeColor = Color.Black;
             codeEditor1.Hotkeys = resources.GetString("codeEditor1.Hotkeys");
             codeEditor1.IsReplaceMode = false;
-            codeEditor1.Location = new Point(0, 20);
-            codeEditor1.Margin = new Padding(3, 2, 3, 2);
+            codeEditor1.Location = new Point(0, 25);
             codeEditor1.Name = "codeEditor1";
             codeEditor1.Paddings = new Padding(0);
             codeEditor1.SelectionColor = Color.FromArgb(60, 0, 0, 255);
             codeEditor1.ServiceColors = (FastColoredTextBoxNS.ServiceColors)resources.GetObject("codeEditor1.ServiceColors");
-            codeEditor1.Size = new Size(329, 99);
+            codeEditor1.Size = new Size(315, 165);
             codeEditor1.TabIndex = 82;
             codeEditor1.Zoom = 100;
             // 
@@ -110,7 +109,7 @@
             label1.ForeColor = Color.DimGray;
             label1.Location = new Point(0, 0);
             label1.Name = "label1";
-            label1.Size = new Size(45, 20);
+            label1.Size = new Size(57, 25);
             label1.TabIndex = 81;
             label1.Text = "Input";
             // 
@@ -129,22 +128,21 @@
     '\'',
     '\''
     };
-            codeEditor2.AutoScrollMinSize = new Size(2, 14);
+            codeEditor2.AutoScrollMinSize = new Size(31, 18);
             codeEditor2.BackBrush = null;
-            codeEditor2.CharHeight = 14;
-            codeEditor2.CharWidth = 8;
+            codeEditor2.CharHeight = 18;
+            codeEditor2.CharWidth = 10;
             codeEditor2.DisabledColor = Color.FromArgb(100, 180, 180, 180);
             codeEditor2.Dock = DockStyle.Fill;
             codeEditor2.ForeColor = Color.Black;
             codeEditor2.Hotkeys = resources.GetString("codeEditor2.Hotkeys");
             codeEditor2.IsReplaceMode = false;
-            codeEditor2.Location = new Point(0, 20);
-            codeEditor2.Margin = new Padding(3, 2, 3, 2);
+            codeEditor2.Location = new Point(0, 25);
             codeEditor2.Name = "codeEditor2";
             codeEditor2.Paddings = new Padding(0);
             codeEditor2.SelectionColor = Color.FromArgb(60, 0, 0, 255);
             codeEditor2.ServiceColors = (FastColoredTextBoxNS.ServiceColors)resources.GetObject("codeEditor2.ServiceColors");
-            codeEditor2.Size = new Size(388, 99);
+            codeEditor2.Size = new Size(323, 165);
             codeEditor2.TabIndex = 84;
             codeEditor2.Zoom = 100;
             // 
@@ -156,7 +154,7 @@
             label2.ForeColor = Color.DimGray;
             label2.Location = new Point(0, 0);
             label2.Name = "label2";
-            label2.Size = new Size(123, 20);
+            label2.Size = new Size(153, 25);
             label2.TabIndex = 83;
             label2.Text = "Expected Output";
             // 
@@ -167,9 +165,10 @@
             button1.FlatAppearance.BorderSize = 0;
             button1.FlatStyle = FlatStyle.Flat;
             button1.ForeColor = Color.Black;
-            button1.Location = new Point(610, 1);
+            button1.Location = new Point(515, 1);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(114, 25);
+            button1.Size = new Size(130, 33);
             button1.TabIndex = 86;
             button1.Text = "Remove";
             button1.UseVisualStyleBackColor = false;
@@ -178,20 +177,22 @@
             // panel1
             // 
             panel1.Controls.Add(button1);
-            panel1.Dock = DockStyle.Top;
-            panel1.Location = new Point(0, 119);
+            panel1.Dock = DockStyle.Bottom;
+            panel1.Location = new Point(0, 190);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(727, 26);
+            panel1.Size = new Size(649, 35);
             panel1.TabIndex = 87;
             // 
             // TestCase2
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(panel1);
             Controls.Add(splitContainer1);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "TestCase2";
-            Size = new Size(727, 151);
+            Size = new Size(649, 225);
             splitContainer1.Panel1.ResumeLayout(false);
             splitContainer1.Panel1.PerformLayout();
             splitContainer1.Panel2.ResumeLayout(false);
