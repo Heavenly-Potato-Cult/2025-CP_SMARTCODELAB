c0089a1080ca2cf4a03632bee804963babc38c6e
StagnantPotato <jimmuelsanggayan@gmail.com>
2025-11-28 14:36:04 +0800

    steam flow layout

---
+++ CustomComponents/SteamThings/SteamFlowPanel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace SmartCodeLab.CustomComponents.SteamThings
+{
+    public class SteamFlowPanel : FlowLayoutPanel
+    {
+        public SteamFlowPanel()
+        {
+          
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint |
+                          ControlStyles.UserPaint |
+                          ControlStyles.OptimizedDoubleBuffer, true);
+
+            
+            this.FlowDirection = FlowDirection.TopDown; 
+            this.WrapContents = false; 
+            this.AutoScroll = true;    
+            this.BackColor = Color.Transparent; 
+        }
+
+       
+        protected override void OnLayout(LayoutEventArgs levent)
+        {
+            base.OnLayout(levent);
+
+            int targetWidth = this.ClientSize.Width - this.Padding.Horizontal;
+
+          
+            targetWidth -= SystemInformation.VerticalScrollBarWidth;
+
+         
+            foreach (Control child in this.Controls)
+            {
+               
+                if (child.Width != targetWidth)
+                {
+                    child.Width = targetWidth;
+
+                  
+                    child.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+                }
+            }
+        }
+
+
+        protected override Point ScrollToControl(Control activeControl)
+        {
+          
+            return this.AutoScrollPosition;
+        }
+    }
+}
