9546cde952d439883bf575b35ba47ad46b921ee1
StagnantPotato <jimmuelsanggayan@gmail.com>
2025-11-28 13:48:03 +0800

    temp fix flickering

--- CustomComponents/ServerPageComponents/ExerciseManagerComponents/SelectExercise.cs
+++ CustomComponents/ServerPageComponents/ExerciseManagerComponents/SelectExercise.cs
@@ -19,15 +19,15 @@ namespace SmartCodeLab.CustomComponents.ServerPageComponents.ExerciseManagerComp
         private System.Threading.Timer refreshTimer;
         private Action<TaskModel> onExerciseSelected;
 
-        protected override CreateParams CreateParams
-        {
-            get
-            {
-                CreateParams cp = base.CreateParams;
-                cp.ExStyle |= 0x02000000; // WS_EX_COMPOSITED
-                return cp;
-            }
-        }
+        //protected override CreateParams CreateParams
+        //{
+        //    get
+        //    {
+        //        CreateParams cp = base.CreateParams;
+        //        cp.ExStyle |= 0x02000000; // WS_EX_COMPOSITED
+        //        return cp;
+        //    }
+        //}
 
         public SelectExercise(Action<TaskModel> onExerciseSelected)
         {
